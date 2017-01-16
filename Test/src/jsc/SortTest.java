48f19864cb546ec4a20b6f7b18bfb8032b0a5f26
Allen0109 <jsc9108@gmail.com>
2017-01-16 14:10:21 +0900

    SortTest First commit

---
+++ Test/src/jsc/SortTest.java
@@ -0,0 +1,15 @@
+package jsc;
+
+public class SortTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]nums={57,5,10,7,63,9};
+		sort(nums,1);
+		sort(nums,2);
+	}
+	private static void sort(int[]nums, int orderby){
+		//,이 부분을 직접 구현
+	}
+
+}
